5b82104a9dec95400fbd67cf465cb7498b08deb6
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-25 14:54:17 +0600

    delete service

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -9,7 +9,7 @@ import logo from '../../../images/logo.png'
 const Header = () => {
     const [user] = useAuthState(auth);
 
-    const handleSignOut = () =>{
+    const handleSignOut = () => {
         signOut(auth);
     }
 
@@ -26,17 +26,24 @@ const Header = () => {
                             <Nav.Link as={Link} to="/">Home</Nav.Link>
                             <Nav.Link href="home#services">Services</Nav.Link>
                             <Nav.Link href="home#experts">Experts</Nav.Link>
-                            
+
                         </Nav>
                         <Nav>
                             <Nav.Link as={Link} to="about">About</Nav.Link>
                             {
+                                user && <>
+                                <Nav.Link as={Link} to="addservice">Add</Nav.Link>
+                                <Nav.Link as={Link} to="manageservice">Manage</Nav.Link>
+
+                                </>
+                            }
+                            {
                                 user ?
                                     <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
-                                :
-                                <Nav.Link as={Link} to="login">
-                                Login
-                            </Nav.Link>}
+                                    :
+                                    <Nav.Link as={Link} to="login">
+                                        Login
+                                    </Nav.Link>}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
